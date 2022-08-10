f802db2c7799dccaca353c7ff6e46f2415e88152
Abouelez <abdo_abouelezz@yahoo.com>
2022-08-10 12:14:34 +0200

    ,

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Catagory;
+use App\Models\Company;
+use App\Models\Incoming;
+use App\Models\Outgoing;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $incoming = Incoming::all()->sum('cost');
+        $outgoing = Outgoing::all()->sum('cost');
+        $products = Product::all()->count();
+        $categories = Catagory::all()->count();
+        $companies = Company::all()->count();
+        return view('mydashboard', ['incoming' => $incoming, 'outgoing' => $outgoing, 'products' => $products, 'categories' => $categories, 'companies' => $companies]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -20,7 +20,7 @@
                     <span class="nav-link">Navigation</span>
                 </li>
                 <li class="nav-item menu-items">
-                    <a class="nav-link" href="{{ route('dashboard') }}">
+                    <a class="nav-link" href="{{ route('dashboard.index') }}">
                         <span class="menu-icon">
                             <i class="mdi mdi-view-dashboard"></i>
                         </span>
